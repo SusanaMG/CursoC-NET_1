44e9e460e3bcfe4c1be645a3538c2574c0eacef5
susanamg <susana.marquez@atos.net>
2021-01-21 19:29:30 +0100

    Temas 1 al 4

---
+++ MessagePrinterSol/MessagePrinter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagePrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable que se usara para guardar el mensaje proporcionado por el usuario
+            string message = String.Empty;
+            string Message = String.Empty; //para probar que es sensible a mayusculas
+
+            Console.WriteLine("Proporciona un mensaje, por favor:");
+            message = Console.ReadLine();
+            if(message != String.Empty)
+            {
+                Console.WriteLine("El mensaje recibido es: '" + message + "'");
+            }
+            else
+            {
+                Console.WriteLine("No se recibió mensaje");
+            }
+            Console.Read();
+        }
+    }
+}
